89b30f6b787ddc927e20880514b66ebb1bad720e
Esteban Correa <estebancorrea405@gmail.com>
2024-03-12 09:55:46 -0500

    Agregando atributo ssl en conexión a bd de psql

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,12 +15,13 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
+      // port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: [ User ],
       subscribers: [  ],
+      ssl: true,
       autoLoadEntities: true,
       synchronize: false
     }),
